88225d7141220a512d8abb713bfa4dafe8f38b71
Abdulrahman Khayat <abdulrahman.khayat@tfarraj.tv>
2024-09-23 22:30:54 +0300

    add user model

---
+++ banking-control-panel/Models/Client.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace banking_control_panel.Models;
+
+public class Client
+{
+    public Guid Id { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+    [MaxLength(60)]
+    public string Firstname { get; set; }
+    [MaxLength(60)]
+    public string Lastname { get; set; }
+    [StringLength(11)]
+    public string PersonalId { get; set; }
+    public string ProfilePhoto { get; set; }
+    [RegularExpression(@"^(\+[1-9]{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$")]
+    public string MobileNumber { get; set; }
+    public SexTypes Sex { get; set; }
+    public Guid AddressId { get; set; }
+    public Address Address { get; set; }
+    [MinLength(1)]
+    public IList<Account> Accounts { get; set; } = new List<Account>();
+}
+
+public enum SexTypes
+{
+    Male,
+    Female
+}
