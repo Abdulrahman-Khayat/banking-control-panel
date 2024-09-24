c7402317038597edbe27bb7df0dee3f5aa31e56f
Abdulrahman Khayat <abdulrahman.khayat@tfarraj.tv>
2024-09-24 22:56:36 +0300

    fix role type in usermodel

--- banking-control-panel/Models/User.cs
+++ banking-control-panel/Models/User.cs
@@ -8,7 +8,6 @@ public class User: BaseModel
     public Guid Id { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
-
     public UserRoles Role { get; set; }
 }
 
