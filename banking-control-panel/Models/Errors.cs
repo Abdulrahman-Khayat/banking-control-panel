596b76805582c02b38b300bfdb04a5f8c428715b
Abdulrahman Khayat <abdulrahman.khayat@tfarraj.tv>
2024-09-24 22:55:33 +0300

    add errors const

---
+++ banking-control-panel/Models/Errors.cs
@@ -0,0 +1,12 @@
+namespace banking_control_panel.Data;
+
+public static class Errors
+{
+    public static string DUPLICATE_EMAIL = "duplicate_email";
+    public static string DUPLICATE_MOBILE = "duplicate_mobile";
+    
+    public static string INVALID_ROLE = "invalid_role";
+    public static string INVALID_SEX = "invalid_sex";
+
+
+}
