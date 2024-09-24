3dec229165139c43af41115fa83eec358e708d3d
Abdulrahman Khayat <abdulrahman.khayat@tfarraj.tv>
2024-09-24 22:59:43 +0300

    update client dto

--- banking-control-panel/Dtos/ClientDto.cs
+++ banking-control-panel/Dtos/ClientDto.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using banking_control_panel.Models;
 
 namespace banking_control_panel.Dtos.UserDto;
 
@@ -8,17 +9,44 @@ public class ReadClientDto
     public string Email { get; set; }
     public string Firstname { get; set; }
     public string Lastname { get; set; }
+    public string PersonalId { get; set; }
+    public string? ProfilePhoto { get; set; }
+    [RegularExpression(@"^(\+[1-9]{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$")]
+    public string MobileNumber { get; set; }
+    public string Sex { get; set; }
+    public ReadAddressDto Address { get; set; }
+    public IList<ReadAccountDto> Accounts { get; set; }
     
 }
 public class CreateClientDto
 {
     [EmailAddress]
     public string Email { get; set; }
-    // [Length(0, 60)]
+    [MaxLength(60)]
     public string Firstname { get; set; }
+    [MaxLength(60)]
     public string Lastname { get; set; }
-    [Length(11, 11, ErrorMessage = "Length must be exactly 11 characters")]
+    [Length(11, 11)]
     public string PersonalId { get; set; }
-    [RegularExpression(@"^(\+[1-9]{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$", ErrorMessage = "Invalid Phone Number format")]
+    public string? ProfilePhoto { get; set; }
+    [RegularExpression(@"^(\+[1-9]{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$")]
     public string MobileNumber { get; set; }
+    public SexTypes Sex { get; set; }
+    public CreateAddressDto Address { get; set; }
+    public IList<CreateAccountDto>? Accounts { get; set; }
+}
+
+public class QueryDto
+{
+    public string? Email { get; set; }
+    [MaxLength(60)]
+    public string? Firstname { get; set; }
+    [MaxLength(60)]
+    public string? Lastname { get; set; }
+    [Length(0, 11)]
+    public string? PersonalId { get; set; }
+    public string? MobileNumber { get; set; }
+
+    public string? OrderBy { get; set; }
+    public SexTypes?  Sex { get; set; }
 }
