305ec58d4129e0d3fa02193ec07c2ee7df75c7a5
Abdulrahman Khayat <abdulrahman.khayat@tfarraj.tv>
2024-09-24 23:02:26 +0300

    add ClientsController.cs

--- banking-control-panel/Controllers/ClientsController.cs
+++ banking-control-panel/Controllers/ClientsController.cs
@@ -1,33 +1,94 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+using System.Security.Claims;
+using banking_control_panel.Data;
 using banking_control_panel.Dtos.UserDto;
+using banking_control_panel.Exceptions;
+using banking_control_panel.Models;
 using banking_control_panel.Services.ClientServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace banking_control_panel.Controllers;
 
 [ApiController]
 [Route("api/v1/clients")]
-public class ClientsController(IClientService _clientService): ControllerBase
+public class ClientsController(IClientService _clientService, ILogger<Client> _logger): ControllerBase
 {
     [HttpGet("{id}", Name = "[controller]/GetById")]
+    [Authorize(Roles = "Admin")]
     public async Task<ActionResult<ReadUserDto>> GetById(Guid id)
     {
-        var result = await _clientService.GetByIdAsync(id);
-        return Ok(result);
+        try
+        {
+            var result = await _clientService.GetByIdAsync(id);
+            return Ok(result);
+        }
+        catch (NotFoundException e)
+        {
+            _logger.LogInformation("Not Found");
+            return NotFound();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return Problem();
+        }
     }
+    
+    
     [HttpGet()]
-    public async Task<ActionResult<PagedResultDto<ReadClientDto>>> GetAll()
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult<PagedResultDto<ReadClientDto>>> GetAll([FromQuery] QueryDto query, [FromQuery] int pageIndex = 1, [FromQuery] int pageSize = 10)
     {
-        var result = await _clientService.GetAll();
+        try
+        {
+            var userId = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Sub).Value;
+            var result = await _clientService.GetAll(new Guid(userId), query, pageIndex, pageSize);
 
-        return Ok(result);
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return Problem();
+        }
     }
     
     [HttpPost()]
+    [Authorize(Roles = "Admin")]
     public async Task<ActionResult<ReadClientDto>> Create(CreateClientDto client)
     {
-        var result = await _clientService.AddAsync(client); 
+        try
+        {
+            var result = await _clientService.AddAsync(client);
+            var route = this.ControllerContext.ActionDescriptor?.ControllerName + "/GetById";
+            return CreatedAtRoute(route, new { result.Id }, result);
+        }
+        catch (ValidationException e)
+        {
+            if (e.Message == Errors.INVALID_SEX)
+            {
+                ModelState.AddModelError("Sex", "Invalid sex type [0: male, 1: female]");
+            }
+            else if (e.Message == Errors.DUPLICATE_EMAIL)
+            {
+                ModelState.AddModelError("Email", "Email already exists");
+            }
+            else if (e.Message == Errors.DUPLICATE_MOBILE)
+            {
+                ModelState.AddModelError("MobileNumber", "MobileNumber already exists");
+            }
+
+            return ValidationProblem();
+        }
+        catch (Exception)
+        {
+            return Problem();
+        }
         
-        var route = this.ControllerContext.ActionDescriptor?.ControllerName + "/GetById";
-        return CreatedAtRoute(route, new{result.Id}, result);
     }
+    
+    
 }
