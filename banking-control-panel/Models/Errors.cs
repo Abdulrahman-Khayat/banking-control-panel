b69cad7042e95b1daf64f66b0362d7ce3d4dde9a
Abdulrahman Khayat <abdulrahman.khayat@tfarraj.tv>
2024-09-25 12:33:14 +0300

    add new errors

--- banking-control-panel/Models/Errors.cs
+++ banking-control-panel/Models/Errors.cs
@@ -4,9 +4,12 @@ public static class Errors
 {
     public static string DUPLICATE_EMAIL = "duplicate_email";
     public static string DUPLICATE_MOBILE = "duplicate_mobile";
+    public static string DUPLICATE_USERNAME = "duplicate_username";
     
     public static string INVALID_ROLE = "invalid_role";
     public static string INVALID_SEX = "invalid_sex";
-
+    public static string INVALID_ACCOUNT = "invalid_account";
+    
+    
 
 }
